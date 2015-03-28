5b76ecf486b79c13b34837f2f2b131f1e4e161e6
maslovaz <audreyslip@gmail.com>
2015-03-28 21:50:03 +0300

    initial commit

---
+++ cd_rent.js
@@ -0,0 +1,98 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+//db.once('open', function (callback) {
+//  // yay!
+//});
+
+//schemas
+var genreSchema = mongoose.Schema({
+	genreName: {
+		type: String,
+		required: true
+		//unique: true,
+	}
+});
+var studioSchema = mongoose.Schema({
+	studioName: {
+		type: String,
+		required: true
+	}
+});
+var actorSchema = mongoose.Schema({
+	actorName: {
+		type: String,
+		required: true
+	},
+	actorSurname: {
+		type: String,
+		required: true
+	},
+	dateOfBirth: {
+		type: Date,
+		required: false
+	}
+});
+var catalogueSchema = mongoose.Schema({
+	movieName: {
+		type: String,
+		required: true
+	},
+	movieRate: {
+		type: Number,
+		required: true,
+		min: 0,
+		max: 10
+	},
+	numberOfCD: {
+		type: Number,
+		required: true,
+		min: 0
+	},
+	numberOfRentedCD: {
+		type: Number,
+		required: true
+	},
+	costPerDay: {
+		type: Number,
+		required: true
+	},
+	mainGenre: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: false
+	},
+	genreList: {
+		type: [mongoose.Schema.Types.ObjectId],
+		required: false
+		//toDo: can be set once
+	},
+	studioList: {
+		type: [mongoose.Schema.Types.ObjectId],
+		required: false
+	},
+	actorList: {
+		type: [mongoose.Schema.Types.ObjectId],
+		required: false
+		//toDo: validate-can be empty
+	},
+	directorList: {
+		type: [mongoose.Schema.Types.ObjectId],
+		required: true
+		//toDo: validate-one is needed or >
+	}
+});
+
+//models
+var Genre = mongoose.model('Genre', genreSchema);
+var Studio = mongoose.model('Studio', studioSchema);
+var Actor = mongoose.model('Actor', actorSchema);
+var Catalogue = mongoose.model('Catalogue', catalogueSchema);
+//var comedy = new Genre({genreName : 'Comedy'});
+
+//exports
+module.exports.Genre = Genre;
+module.exports.Studio = Studio;
+module.exports.Actor = Actor;
+module.exports.Catalogue = Catalogue;
+
